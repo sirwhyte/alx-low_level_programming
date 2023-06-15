ec280d644e3977952d5c1c5ff69a42566ef2328d
sirwhyte <ubietuchristian@gmail.com>
2023-06-15 15:45:28 -0700

    task 5 c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -S; puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle , followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+     puts ("/'"programming is like a multilingual puzzle);
+
+
+	return (0);
+}		
