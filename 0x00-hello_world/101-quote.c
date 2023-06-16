8b98b311d24687c230bc5036b8597d16af0e4e53
sirwhyte <ubietuchristian@gmail.com>
2023-06-15 18:30:29 -0700

    task 8 c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-        fwrite("and that piece of art is useful"" - Dora Korpar, 2015-10-19,\n");
+        write("and that piece of art is useful,\n");(" - Dora Korpar,\n");
+	       ("2015-10-19,\n");
         return (1);
 }
