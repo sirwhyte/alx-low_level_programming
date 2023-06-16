80c7b29867e36120a0c567fdde57263064aa9c64
sirwhyte <ubietuchristian@gmail.com>
2023-06-16 08:22:45 -0700

    task 0 c variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main = Positive anything is better than Negative Nothing
+ * Description: Assigning random number to variable n.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive,\n")
+	}
+	if (n == 0)
+	{
+		printf("is zero,\n")
+	}
+	if (n < 0)
+	{
+		printf("is negative,\n")
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+my ninth readme
