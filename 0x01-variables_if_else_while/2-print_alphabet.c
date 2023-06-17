46b112010e5017298123d75623ff08803f6c2f34
sirwhyte <ubietuchristian@gmail.com>
2023-06-16 19:17:15 -0700

    task 2 c variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -  I sometimes suffer from insomnia.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = a; a <= a--z; a++)
+	{
+		putchar(a + 'a');
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
