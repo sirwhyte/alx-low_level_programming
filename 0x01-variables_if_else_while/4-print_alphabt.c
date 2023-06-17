d2c4b76fce4101271eb511e9ef6b1698e8e70f19
sirwhyte <ubietuchristian@gmail.com>
2023-06-16 17:01:05 -0700

    task 2 c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - When I was having that alphabet soup, I never thought that it would pay off
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		write(letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n')
+	return (0);
+}
