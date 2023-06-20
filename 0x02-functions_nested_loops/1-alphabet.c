dddbbc029c76cfa296f41052eefed7f15cdc579c
sirwhyte <ubietuchristian@gmail.com>
2023-06-20 05:34:00 -0700

    the 2nd main push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main -  I sometimes suffer from insomnia
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while  (ch <= 'z');
+	{
+		_putchar(char ch);
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
