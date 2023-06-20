a1644a84780d59e18b7642fab568ac858cd13fbb
sirwhyte <ubietuchristian@gmail.com>
2023-06-20 14:13:53 -0700

    2nd main push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 /**
  * main -  I sometimes suffer from insomnia
  * Description: i play the alphabet game
+ * print_alphabet - prints lower case alphabet
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
