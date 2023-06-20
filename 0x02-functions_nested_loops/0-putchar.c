fa28653638410ba8eaa4be40af6c3cca8219bfa4
sirwhyte <ubietuchristian@gmail.com>
2023-06-19 23:47:00 -0700

    my first self programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Functions And Nested Loops
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	putchar("_putchar");
+
+	putchar ('\n');
+	return (0);
+}
