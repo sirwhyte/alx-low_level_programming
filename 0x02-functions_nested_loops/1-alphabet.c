29a8e411dfd6d88af0b1890cd4c0c678adfe5dea
sirwhyte <ubietuchristian@gmail.com>
2023-06-20 14:27:14 -0700

    2nd main push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,8 @@
  * print_alphabet - prints lower case alphabet
  * Return: Always 0 (Success)
  */
-void print_alphabet(void):bhsbhbchcdhbcd
+print_alphabets of lowercase
+void print_alphabet(void)
 {
 	char ch;
 
