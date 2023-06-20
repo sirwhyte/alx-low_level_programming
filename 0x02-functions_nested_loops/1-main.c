70eeb14860f922df503a9ed39900d3bea51cfc3f
sirwhyte <ubietuchristian@gmail.com>
2023-06-20 06:20:50 -0700

    2nd main push

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,1 +1,13 @@
-void print_alphabet(void);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: on Success 0.
+ * on error, 1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+        return (write(0, abc, 1));
+}
