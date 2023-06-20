9c7a80af4a94d70bd5109b85f355067dbd6332de
sirwhyte <ubietuchristian@gmail.com>
2023-06-20 14:09:57 -0700

    the alphabets

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -3,6 +3,7 @@
 /**
  * _putchar - writes the character ch to stdout
  * @ch: The character to print
+ * print_alphabet: prints the lower case alphabet
  * Return: on Success 0.
  * on error, 0 is returned, and errno is set appropriately.
  */
