04f93800204d14c8762f33f8539349714bd3c15e
sirwhyte <ubietuchristian@gmail.com>
2023-06-24 00:31:33 -0700

    4th main push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lower case alphabets
+ * _islower - Detects lowercase and uppercase
  *
  * @c: is a parameter
  * Return: (1) if c is lower otherwise (0)
@@ -11,11 +11,11 @@ int _islower(int c)
 	char ch;
 	int lower = 0;
 
-	for (ch = 'a'; ch <= 'z'' ch++)
+	for (ch = 'a'; ch <= 'z' ch++)
 	{
 		if (ch == c)
 		{
-			lower = 1;
+		lower = 1;
 		}
 	}
 		return (lower);
