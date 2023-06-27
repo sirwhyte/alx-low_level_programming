810e6d26ab5129b2fc7c69e2f52a16a3759610fe
sirwhyte <ubietuchristian@gmail.com>
2023-06-27 07:24:04 -0700

    the greta things

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,21 +4,10 @@
 /**
  * reset_to_98 - takes all value with a pointer
  * Description: helps with increasing pointers
- * @*n: variables
+ * @n: integer
  * Return: Always 0 (Success)
  */
-void reset_to_98(int n)
+void reset_to_98(int *n)
 {
-	int n = 78;
-	int *ptr_n = &n;
-
-	{
-		printf("the value of n before dereferencing is:  %d\n", n);
-		
-		*ptr_n = 98;
-		
-		printf("the value of n after dereferencing is: %d\n", n);
-	}
-
-	return (0);
+	*n = 98;
 }
