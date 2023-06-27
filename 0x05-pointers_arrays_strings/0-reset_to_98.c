46099c7086c65276f1eedddff5c5fccfd18c3dca
sirwhyte <ubietuchristian@gmail.com>
2023-06-27 06:39:25 -0700

    the greta things

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - takes all value with a pointer
+ * Description: helps with increasing pointers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 78;
+	int *ptr_num = &num;
+
+	printf("the value of num before dereferencing is:  %d\n", num);
+
+	*ptr_num = 98
+
+	printf("the value of num after dereferencing is: %d\n", num);
+
+	return (0);
+}
