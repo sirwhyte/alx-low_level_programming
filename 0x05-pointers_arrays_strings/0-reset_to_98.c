9cc094c160cd97bd33f7590a7cb8052baab4b87d
sirwhyte <ubietuchristian@gmail.com>
2023-06-27 06:49:21 -0700

    the greta things

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,11 +10,13 @@ int main(void)
 	int num = 78;
 	int *ptr_num = &num;
 
-	printf("the value of num before dereferencing is:  %d\n", num);
-
-	*ptr_num = 98;
-
-	printf("the value of num after dereferencing is: %d\n", num);
+	{
+		printf("the value of num before dereferencing is:  %d\n", num);
+		
+		*ptr_num = 98;
+		
+		printf("the value of num after dereferencing is: %d\n", num);
+	}
 
 	return (0);
 }
