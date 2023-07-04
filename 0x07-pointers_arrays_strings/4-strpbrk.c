46765e957b354a6c41fc3c630ccf2f07df4375e8
sirwhyte <ubietuchristian@gmail.com>
2023-07-03 22:23:30 -0700

    special commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,7 +16,7 @@ char *_strpbrk(char *s, char *accept)
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
-					return (s + i);
+				return (s + i);
 					}
 		}
 
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints characters
+ * @c: character
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	_putchar(char c);
+}
